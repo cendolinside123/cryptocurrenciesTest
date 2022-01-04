0bf4c64bc5ff73a1cd1662f223dee48bfe0c35e5
jan <jan@code.id>
2022-01-05 00:26:04 +0700

    add/change: - add NewsUseCase dan melakukan implementasi dari abstract NewsNetworkProvider - menambahkan proses API request untuk mendapatkan list berita

---
+++ CrptoInfo/Controller/News/NewsUseCases/NewsUseCase.swift
@@ -0,0 +1,46 @@
+//
+//  NewsUseCase.swift
+//  CrptoInfo
+//
+//  Created by Mac on 04/01/22.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+struct NewsUseCase {
+    
+}
+
+extension NewsUseCase: NewsNetworkProvider {
+    
+    struct NewsGETParams : Codable {
+        let lang: String
+        let categories: String
+    }
+    
+    func fetchNews(category: String, completion: @escaping (NetworkResult<[News]>) -> Void) {
+        
+        let params: NewsGETParams = NewsGETParams(lang: "EN", categories: category)
+        
+        AF.request("https://min-api.cryptocompare.com/data/v2/news/", method: .get, parameters: params, encoder: URLEncodedFormParameterEncoder.default, headers: nil, interceptor: nil, requestModifier: nil).responseJSON(completionHandler: { response in
+            switch response.result {
+            case .success(let data):
+                let getJSON = JSON(data)
+                let getData = getJSON["data"].arrayValue
+                var listBerita = [News]()
+                for item in getData {
+                    listBerita.append(News(json: item))
+                }
+                completion(.success(listBerita))
+                
+            case .failure(let error):
+                completion(.failed(error))
+            }
+        })
+    }
+    
+    
+}
+
