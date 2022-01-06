5d891184823c7eab0dfe1db61855a98def6b1e88
jan <jan@code.id>
2022-01-06 12:23:45 +0700

    add/change: - fix variable naming on struct Coin - make new struct UsdCurency and move some property form struct Coin to UsdCurency

--- CrptoInfo/Model/Coin.swift
+++ CrptoInfo/Model/Coin.swift
@@ -6,24 +6,51 @@
 //
 
 import Foundation
-struct Coin {
-    let Id: String
-    let Name: String
-    let FullName: String
-    let Internal: String
+import SwiftyJSON
+
+struct UsdCurency {
     let fromsymbol: String
-    let tosymbol: String
+    let toSymbol: String
     let market: String
     var price: Double
     let lastmarket: String
-    let openDay: Double
+    var openDay: Double
     var change: String
     var changePercent: String
-    var toSymbol: String
 }
-extension Coin {
+extension UsdCurency {
+    
+    init(json: JSON) {
+        fromsymbol = json["DISPLAY"]["USD"]["FROMSYMBOL"].stringValue
+        toSymbol = json["DISPLAY"]["USD"]["TOSYMBOL"].stringValue
+        market = json["RAW"]["USD"]["MARKET"].stringValue
+        price = json["RAW"]["USD"]["MARKET"].doubleValue
+        lastmarket = json["DISPLAY"]["USD"]["LASTMARKET"].stringValue
+        openDay = json["RAW"]["USD"]["OPENDAY"].doubleValue
+        change = "\(price - openDay)"
+        changePercent = "\(((price - openDay)/openDay) * 100)%"
+    }
+    
     mutating func countChange() {
         change = "\(price - openDay)"
         changePercent = "\(((price - openDay)/openDay) * 100)%"
+    }
+}
+
+struct Coin {
+    let id: String
+    let name: String
+    let fullName: String
+    let _internal: String
+    let usdCurency: UsdCurency
+}
+extension Coin {
+    
+    init(json: JSON) {
+        id = json["CoinInfo"]["Id"].stringValue
+        name = json["CoinInfo"]["Name"].stringValue
+        fullName = json["CoinInfo"]["FullName"].stringValue
+        _internal = json["CoinInfo"]["Internal"].stringValue
+        usdCurency = UsdCurency(json: json)
     }
 }
