f75b333e56394b4c04e747cc708f4e73ab1a01d0
jan <jan@code.id>
2022-01-06 09:53:30 +0700

    add/change: - add property on struct Coin - add mutabling function named countChange() for change value property change and changePercent

--- CrptoInfo/Model/Coin.swift
+++ CrptoInfo/Model/Coin.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+struct Coin {
+    let Id: String
+    let Name: String
+    let FullName: String
+    let Internal: String
+    let fromsymbol: String
+    let tosymbol: String
+    let market: String
+    var price: Double
+    let lastmarket: String
+    let openDay: Double
+    var change: String
+    var changePercent: String
+    var toSymbol: String
+}
+extension Coin {
+    mutating func countChange() {
+        change = "\(price - openDay)"
+        changePercent = "\(((price - openDay)/openDay) * 100)%"
+    }
+}
