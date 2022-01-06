49c8cfb2188ccc672bcef03447c7c933b4c3812f
jan <jan@code.id>
2022-01-06 18:43:05 +0700

    add/change: - initiate ListViewController as root view controller

--- CrptoInfo/AppDelegate.swift
+++ CrptoInfo/AppDelegate.swift
@@ -15,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        let newsVC = ListCoinViewController()
+        let nav = UINavigationController(rootViewController: newsVC)
+        nav.navigationBar.barTintColor = .white
+        
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
         return true
     }
 
