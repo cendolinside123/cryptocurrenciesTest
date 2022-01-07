c213c14e19cdd7ba3063d15b42480b919b2d21c7
jan <jan@code.id>
2022-01-07 12:09:53 +0700

    add/change: - add few method/function for websocket implementation on abstract class

--- CrptoInfo/Controller/Coin/ViewModel/CoinGuideline.swift
+++ CrptoInfo/Controller/Coin/ViewModel/CoinGuideline.swift
@@ -9,6 +9,10 @@ import Foundation
 
 protocol CoinGuideline {
     func loadCoins(limit: Int, tsym: String, reloadTime: Int)
+    func coinsWebSocket(coins: [Coin], toCurency: String, retryTime: Int)
+    func disconect()
     var coinResult: (([Coin]) -> Void)? { get set }
     var fetchError: ((String) -> Void)? { get set }
+    var webSocketError: ((String) -> Void)? { get set }
+    var webSocketResponse: (([String : Any]) -> Void)? { get set }
 }
