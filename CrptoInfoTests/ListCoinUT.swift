4f0cf89fea03e616a6b6904af0bcfc44ccecb05f
Jan Sebastian <jnsbstn391@gmail.com>
2022-05-11 00:07:34 +0700

    fix:
    - fix UT for testSucessLoad_offlineTest()
    
    add:
    
    - add new UT case testRefetch_offlineTest() (case if user refetch list coin and there're new coin from server)
    - add new UT case testRefetch_offlineTestV2 (case if user refetch list coin and server remove any coin from their list)

--- CrptoInfoTests/ListCoinUT.swift
+++ CrptoInfoTests/ListCoinUT.swift
@@ -11,29 +11,38 @@ import XCTest
 class ListCoinUT: XCTestCase {
 
     func testSucessLoad_offlineTest() {
-        var viewModel: CoinGuideline = CoinViewModel(useCase: CointUseCase(cointDataSource: CoinUseCase(), coinDataRealtime: nil))
+        var viewModel: CoinGuideline = CoinViewModel(useCase: CointUseCase(cointDataSource: SuccessCoinListUseCase(), coinDataRealtime: nil))
         let expectedResult = MockData.generateCoinList()
         let expect = expectation(description: "Should return coin data")
         
         viewModel.coinResult = { result in
-            var indexLoop = 0
-            for responseItem in result {
-                
-                XCTAssertEqual(responseItem.id, expectedResult[indexLoop].id)
-                XCTAssertEqual(responseItem.name, expectedResult[indexLoop].name)
-                XCTAssertEqual(responseItem.fullName, expectedResult[indexLoop].fullName)
-                XCTAssertEqual(responseItem._internal, expectedResult[indexLoop]._internal)
-                XCTAssertEqual(responseItem.curency.fromsymbol, expectedResult[indexLoop].curency.fromsymbol)
-                XCTAssertEqual(responseItem.curency.toSymbol, expectedResult[indexLoop].curency.toSymbol)
-                XCTAssertEqual(responseItem.curency.openDay, expectedResult[indexLoop].curency.openDay)
-                XCTAssertEqual(responseItem.curency.price, expectedResult[indexLoop].curency.price)
-                XCTAssertEqual(responseItem.curency.change, expectedResult[indexLoop].curency.change)
-                XCTAssertEqual(responseItem.curency.changePercent, expectedResult[indexLoop].curency.changePercent)
-                XCTAssertEqual(responseItem.curency.market, expectedResult[indexLoop].curency.market)
-                XCTAssertEqual(responseItem.curency.lastmarket, expectedResult[indexLoop].curency.lastmarket)
-                
-                indexLoop = indexLoop + 1
-            }
+            XCTAssertEqual(viewModel.listCoin, expectedResult)
+            expect.fulfill()
+        }
+        viewModel.loadCoins(limit: 1, tsym: "USD", reloadTime: 3)
+        wait(for: [expect], timeout: 1)
+    }
+    
+    func testRefetch_offlineTest() {
+        var viewModel: CoinGuideline = CoinViewModel(useCase: CointUseCase(cointDataSource: SuccessCoinListUseCaseV2(), coinDataRealtime: nil))
+        let expectedResult = MockData.generateCoinListV2()
+        let expect = expectation(description: "Should return coin data")
+        viewModel.listCoin = MockData.generateCoinList()
+        viewModel.coinResult = { result in
+            XCTAssertEqual(viewModel.listCoin, expectedResult)
+            expect.fulfill()
+        }
+        viewModel.loadCoins(limit: 2, tsym: "USD", reloadTime: 3)
+        wait(for: [expect], timeout: 1)
+    }
+    
+    func testRefetch_offlineTestV2() {
+        var viewModel: CoinGuideline = CoinViewModel(useCase: CointUseCase(cointDataSource: SuccessCoinListUseCase(), coinDataRealtime: nil))
+        let expectedResult = MockData.generateCoinList()
+        let expect = expectation(description: "Should return coin data")
+        viewModel.listCoin = MockData.generateCoinListV2()
+        viewModel.coinResult = { result in
+            XCTAssertEqual(viewModel.listCoin, expectedResult)
             expect.fulfill()
         }
         viewModel.loadCoins(limit: 1, tsym: "USD", reloadTime: 3)
@@ -41,7 +50,7 @@ class ListCoinUT: XCTestCase {
     }
     
     func testFailedLoad_offlineTest() {
-        var viewModel: CoinGuideline = CoinViewModel(useCase: CointUseCase(cointDataSource: CoinUseCase(), coinDataRealtime: nil))
+        var viewModel: CoinGuideline = CoinViewModel(useCase: CointUseCase(cointDataSource: FailedCoinListUseCase(), coinDataRealtime: nil))
         let expect = expectation(description: "Should failed load")
         
         viewModel.fetchError = { errorMessage in
